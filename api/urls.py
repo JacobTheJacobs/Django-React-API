4202df15ecbda2778c7b816407fbcc2a1e3b9222
unknown <jake.yanov@gmail.com>
2020-10-12 00:02:17 +0300

    auth implemented

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
-from .views import MovieViewSet,RatingViewSet
+from .views import MovieViewSet,RatingViewSet,UserViewSet
 
 
 router=routers.DefaultRouter()
 
+router.register('users', UserViewSet, 'users')
 router.register('movies', MovieViewSet, 'movies')
 router.register('ratings', RatingViewSet, 'ratings')
 
--- api/views.py
+++ api/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from .models import Movie, Rating
-from .serializer import MovieSerializer, RatingSerializer
+from .serializer import MovieSerializer, RatingSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
@@ -18,8 +27,8 @@ class MovieViewSet(viewsets.ModelViewSet):
 
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            user = User.objects.get(id=1)
-
+            user = request.user
+            # update UPDATE
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
                 rating.stars = stars
@@ -27,7 +36,7 @@ class MovieViewSet(viewsets.ModelViewSet):
                 serializer = RatingSerializer(rating, many=False)
                 response = {'message': 'Rating updated', 'result': serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
-
+            # create POST
             except:
                 rating = Rating.objects.create(user=user, movie=movie, stars=stars)
                 serializer = RatingSerializer(rating, many=False)
@@ -40,7 +49,17 @@ class MovieViewSet(viewsets.ModelViewSet):
             response = {'message': 'You need to provide rating'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You cant update rating'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     serializer_class = RatingSerializer
     queryset = Rating.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
